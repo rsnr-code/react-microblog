c0c011c2e83183c65a360b7c5ec918884dabd8ac
rsnr-code <rishadnero@gmail.com>
2022-10-13 18:03:49 -0400

    added catch error capability

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetch("http://localhost:8000/blogs")
+        .then(res => {
+            if(!res.ok) {
+                throw Error('could not fetch data...')
+            }
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+        })  
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+        })
+    }, [])
 
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) =>  blog.author === 'mario')} title="Mario's Blogs" />
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 }
