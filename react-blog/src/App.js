a60a73597d3c8244376ae1c095954af395b2ae19
rsnr-code <rishadnero@gmail.com>
2022-10-14 14:21:54 -0400

    added delete blog and 404 page

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,11 +1,11 @@
-import Navbar from './Navbar'
-import Home from './Home'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
- 
   return (
     <Router>
       <div className="App">
@@ -21,11 +21,13 @@ function App() {
             <Route path="/blogs/:id">
               <BlogDetails />
             </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
           </Switch>
         </div>
       </div>
     </Router>
-    
   );
 }
 
